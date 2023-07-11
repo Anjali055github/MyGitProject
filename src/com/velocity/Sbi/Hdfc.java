94e6bafbabf55c79e0b3bd13669a82820cc6074c
Anjali Jadhav <anjali.javadeveloper55@gmail.com>
2023-07-11 23:19:32 +0530

    Added hdfc class file

---
+++ src/com/velocity/Sbi/Hdfc.java
@@ -0,0 +1,10 @@
+package com.velocity.Sbi;
+
+public class Hdfc {
+
+	public static void main(String[] args) {
+		System.out.println("Hdfc bank");
+
+	}
+
+}
--- src/com/velocity/Sbi/Sbi.java
+++ src/com/velocity/Sbi/Sbi.java
@@ -6,6 +6,7 @@ public class Sbi {
 		// TODO Auto-generated method stub
 
 		System.out.println("Sbi Bank");
+		
 	}
 
 }
